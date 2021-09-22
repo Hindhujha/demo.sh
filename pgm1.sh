a6bd2ef03cf6cf62de453044c0cf931b68bf16a4
unknown <hindhujhasridhar@gmail.com>
2021-09-22 20:06:05 +0530

    sum of numbers using for loop

--- pgm1.sh
+++ pgm1.sh
@@ -1,1 +1,15 @@
-echo first
+#!/bin/bash -x
+i=1
+sum=0
+echo enter a limit of numbers to add
+read N
+
+while [ $i -le $N ]
+do
+echo enter the numbers
+read num
+sum=$(( sum + num ))
+i=$((i + 1))
+done
+echo sum is $sum
+
